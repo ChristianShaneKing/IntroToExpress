62e12bc4c2ed0eccb691fcd0536be2fa4a62e738
Christian King <christianshaneking@gmail.com>
2019-08-13 02:17:53 +0000

    modify app.js file

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,1 +1,16 @@
-console.log("This is where our express app goes!!!")
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res){
+	res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye!"
+// "/dog" => "MEOW!"
+
+
+// Tell express to listen for requests (start server)
+app.listen(3000, function(){
+	console.log("Server has started!");
+});
