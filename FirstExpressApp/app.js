9973a49782dbf84af2f48733ae935603a8212c8c
Christian King <christianshaneking@gmail.com>
2019-08-14 02:39:53 +0000

    modify app.js file

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -7,7 +7,16 @@ app.get("/", function(req, res){
 });
 
 // "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+	res.send("Goodbye!");
+});
+
 // "/dog" => "MEOW!"
+app.get("/dog", function(req, res){
+	console.log("Someone made a request to /DOG!!!")
+	res.send("MEOW!");
+});
+
 
 
 // Tell express to listen for requests (start server)
