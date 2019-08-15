a30d9f92d5e02e4ca734fdcd4e332bacee7e079b
Christian King <christianshaneking@gmail.com>
2019-08-15 03:42:37 +0000

    modify app.js file

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -17,6 +17,19 @@ app.get("/dog", function(req, res){
 	res.send("MEOW!");
 });
 
+app.get("/r/:subredditName", function(req, res){
+	var subreddit = req.params.subredditName;
+	res.send("Welcome to the " + subreddit + " subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+	console.log(req.params);
+	res.send("Welcome to the COMMENTS page!");
+});
+
+app.get("*", function(req, res){
+	res.send("YOU ARE A STAR!!!");
+});
 
 
 // Tell express to listen for requests (start server)
